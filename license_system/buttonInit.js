45b5f7daeeccb65639c93121cc017d317e9902dd
GG-Yeh <b113040026@g-mail.nsysu.edu.tw>
2024-12-10 08:22:11 +0800

    Update buttonInit.js

--- license_system/buttonInit.js
+++ license_system/buttonInit.js
@@ -1,31 +1,31 @@
 function listUserButton() {
-    document.write(`<a class="btn btn-dark m-1" href="User/userList.html" role="button">List</a>`)
+    document.write(`<a class="btn btn-dark m-1" href="license_system/User/userList.html" role="button">List</a>`)
 }
 
 function addUserButton() {
-    document.write(`<a class="btn btn-primary m-1" href="User/addUser.html" role="button">Add</a>`)
+    document.write(`<a class="btn btn-primary m-1" href="license_system/User/addUser.html" role="button">Add</a>`)
 }
 
 function updateUserButton() {
-    document.write(`<a class="btn btn-info m-1" href="User/updateUser.html" role="button">Update</a>`)
+    document.write(`<a class="btn btn-info m-1" href="license_system/User/updateUser.html" role="button">Update</a>`)
 }
 
 function deleteUserButton() {
-    document.write(`<a class="btn btn-danger m-1" href="User/deleteUser.html" role="button">Delete</a>`)
+    document.write(`<a class="btn btn-danger m-1" href="license_system/User/deleteUser.html" role="button">Delete</a>`)
 }
 
 function listViolationButton() {
-    document.write(`<a class="btn btn-dark m-1" href="Violation/violationList.html" role="button">List</a>`)
+    document.write(`<a class="btn btn-dark m-1" href="license_system/Violation/violationList.html" role="button">List</a>`)
 }
 
 function addViolationButton() {
-    document.write(`<a class="btn btn-primary m-1" href="Violation/addViolation.html" role="button">Add</a>`)
+    document.write(`<a class="btn btn-primary m-1" href="license_system/Violation/addViolation.html" role="button">Add</a>`)
 }
 
 function updateViolationButton() {
-    document.write(`<a class="btn btn-info m-1" href="Violation/updateViolation.html" role="button">Update</a>`)
+    document.write(`<a class="btn btn-info m-1" href="license_system/Violation/updateViolation.html" role="button">Update</a>`)
 }
 
 function deleteViolationButton() {
-    document.write(`<a class="btn btn-danger m-1" href="Violation/deleteViolation.html" role="button">Delete</a>`)
+    document.write(`<a class="btn btn-danger m-1" href="license_system/Violation/deleteViolation.html" role="button">Delete</a>`)
 }

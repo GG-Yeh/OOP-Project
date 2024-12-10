60ac3330194bf278c69a357a3ee131fb64548dfe
GG-Yeh <b113040026@g-mail.nsysu.edu.tw>
2024-12-10 10:05:15 +0800

    Update app.py

--- license_system/app.py
+++ license_system/app.py
@@ -14,10 +14,11 @@ CORS(app)  # 允許所有來源的跨來源請求
 BASE_URL = os.getenv("API_BASE_URL")  # 從 .env 檔案中讀取
 
 @app.route('/', methods=['GET', 'HEAD'])
-def root():
+def serve_index():
     if request.method == 'HEAD':
         return '', 200  # HEAD 請求只需要返回標頭即可
-    return "Welcome to the API!", 200
+    # 返回 index.html 文件
+    return send_from_directory('.', 'index.html')
 
 @app.route('/api/users', methods=['GET'])
 def get_all_users():

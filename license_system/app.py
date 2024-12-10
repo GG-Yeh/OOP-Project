315724ea0e8447488922f1e00482c5c97b5bc5cc
GG-Yeh <b113040026@g-mail.nsysu.edu.tw>
2024-12-10 09:34:45 +0800

    Update app.py

--- license_system/app.py
+++ license_system/app.py
@@ -1,3 +1,4 @@
+'''
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import requests
@@ -76,3 +77,14 @@ def delete_user(user_id: str):
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 10000))  # 預設埠號為 10000
     app.run(debug=True, host='0.0.0.0', port=port)
+'''
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

5a5b9a3bfaca95e27e96cfa4befb7c0c9f0cb08d
GG-Yeh <b113040026@g-mail.nsysu.edu.tw>
2024-12-10 16:28:31 +0800

    Update addUser.js

--- User/addUser.js
+++ User/addUser.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
 
             // Send data to the backend
-            fetch('http://127.0.0.1:10000/api/users', {
+            fetch('https://oop-project-w9ul.onrender.com/api/users', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
